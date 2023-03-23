eb0621778d37ee8fce96bb4684af1b31f62e73e6
Dmytro <dimores95@gmail.com>
2023-03-23 12:53:52 +0200

    company screen completed

--- src/js/files/script.js
+++ src/js/files/script.js
@@ -2,3 +2,23 @@
 import { isMobile } from "./functions.js";
 // Підключення списку активних модулів
 import { flsModules } from "./modules.js";
+
+let player = document.querySelector('.video');
+let playBtn = document.querySelector('.video__btn');
+let playBtnWrap = document.querySelector('.video__btn-wrp');
+
+
+playBtn.addEventListener('click', function() {
+    player.play();
+});
+
+player.addEventListener('play', function() {
+    playBtnWrap.style.display = 'none';
+    document.querySelector('.about__img').style.display = 'none';
+    player.setAttribute("controls", "controls");
+})
+
+player.addEventListener('pause', function() {
+    playBtnWrap.style.display = 'flex';
+    document.querySelector('.about__img').style.display = 'inline-block'
+})
