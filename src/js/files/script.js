dc2898b0d06bde0928a23cc9db0315587c8a32c2
Dmytro <dimores95@gmail.com>
2023-03-23 16:02:45 +0200

    added language switcher

--- src/js/files/script.js
+++ src/js/files/script.js
@@ -8,17 +8,37 @@ let playBtn = document.querySelector('.video__btn');
 let playBtnWrap = document.querySelector('.video__btn-wrp');
 
 
-playBtn.addEventListener('click', function() {
+playBtn?.addEventListener('click', function() {
     player.play();
 });
 
-player.addEventListener('play', function() {
+player?.addEventListener('play', function() {
     playBtnWrap.style.display = 'none';
     document.querySelector('.about__img').style.display = 'none';
     player.setAttribute("controls", "controls");
 })
 
-player.addEventListener('pause', function() {
+player?.addEventListener('pause', function() {
     playBtnWrap.style.display = 'flex';
     document.querySelector('.about__img').style.display = 'inline-block'
+})
+
+let languageSwitcher = document.querySelector('#languageSwitcher');
+let open = false;
+
+function isOpen(){
+    if(open)
+       return document.querySelector('.header__switcher_wrap')?.classList.toggle('_open');
+    else
+       return document.querySelector('.header__switcher_wrap')?.classList.toggle('_open');
+}
+
+languageSwitcher?.addEventListener('click', function() {
+    open = !open;
+    isOpen();
+})
+
+languageSwitcher?.addEventListener('blur', function() {
+    open = !open;
+    isOpen();
 })
